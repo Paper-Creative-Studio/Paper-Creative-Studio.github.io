1a687b784a0f05faf9658bb8eef2b5a366251d88
Hubert <57830712+2bombel2@users.noreply.github.com>
2023-03-17 19:05:21 +0100

    fixed nav + team create

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ document.addEventListener('DOMContentLoaded', (e)=>{
 
     }, 2000)
 })
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
